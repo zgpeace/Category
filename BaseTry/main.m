d9e3677ce1c2e253ccb00a215b84b8eb1b90dbeb
cbz <cbz@cbzdeMacBook-Pro.local>
2015-08-08 17:15:27 +0800

    Initial Commit

---
+++ BaseTry/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BaseTry
+//
+//  Created by cbz on 8/8/15.
+//  Copyright (c) 2015 zgpeace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
